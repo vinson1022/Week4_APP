59b02b3fef80a61efd6409383265996e876afe93
vinson1022 <vinson510123@gmail.com>
2015-12-15 17:01:28 +0800

    [UPDATE] restart v1

---
+++ app/src/main/java/com/example/vinson_chen/week4_app/ThreeFragment.java
@@ -0,0 +1,30 @@
+package com.example.vinson_chen.week4_app;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vinson_Chen on 2015/12/15.
+ */
+public class ThreeFragment extends Fragment {
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
